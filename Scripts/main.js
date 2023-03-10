38cbc5a759c75e367a8870948a323900dcc5acba
GuillePfarr <guillepfarr@hotmail.com>
2023-03-10 17:49:52 -0300

    Formulario dinámico

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,40 +1,29 @@
-events = data.events
+  
 
-const contenedorTarjetas = document.querySelector("#contenedor")
-
-let tarjetasGeneradas = crearTarjetas(events)
-
-contenedorTarjetas.innerHTML = tarjetasGeneradas
-
-function crearTarjetas(arrayData){
-    let tarjetas =""
-    for (const event of arrayData){
-        tarjetas += `<div class="card">
-        <img src="${event.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${event.name}</h5>
-          <p class="card-text">${event.description}</p>
-          <div class="card-footer">
-          <p>Price:&#36; ${event.price}</p>
-          <input type="button" class="btn btn-warning" onclick="seeDetail('${event._id}')" value="See more" id="button"></input>
-          
-        </div>
-        </div>
-      </div>`
-    }
-    return tarjetas
+function crearTarjetas( data ){
+  let tarjetas =""   
+  for (const event of data){
+      tarjetas += `<div class="card">
+      <img src="${event.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${event.name}</h5>
+        <p class="card-text">${event.description}</p>
+        <div class="card-footer">
+        <p>Price:&#36; ${event.price}</p>
+        <input type="button" class="btn btn-warning" onclick="seeDetail('${event._id}')" value="See more" id="button"></input>
+        
+      </div>
+      </div>
+    </div>`
+  }
+  document.querySelector("#contenedor").innerHTML = tarjetas
 }
 
 function seeDetail(_id) {
-  window.location.href = `./details.html?id=${_id}`
+window.location.href = `./details.html?id=${_id}`
 }
-
-{/* <a href="details.html" class="btn btn-warning">DETAILS</a> */}
-
-
-
-
-
+let ev = data.events
+crearTarjetas( ev )
 
 
 
--- Scripts/pevents.js
+++ Scripts/pevents.js
@@ -9,9 +9,9 @@ let tarjetasGeneradas = crearTarjetas(p_events)
 
 contenedorTarjetas.innerHTML = tarjetasGeneradas
 
-function crearTarjetas(arrayData){
+function crearTarjetas( data ){
     let tarjetas =""
-    for (const event of arrayData){
+    for (const event of data){
         tarjetas += `<div class="card">
         <img src="${event.image}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -20,16 +20,18 @@ function crearTarjetas(arrayData){
           <p class="card-text">${event.description}</p>
           <div class="card-footer">
           <p>Price:&#36; ${event.price}</p>
-          
           <input type="button" class="btn btn-warning" onclick="seeDetail('${event._id}')" value="See more" id="button"></input>
           
         </div>
         </div>
       </div>`
     }
+    document.querySelector("#contenedor").innerHTML = tarjetas
     return tarjetas
 }
 
 function seeDetail(_id) {
   window.location.href = `./details.html?id=${_id}`
 }
+// let ev = data.events
+// crearTarjetas( ev )
