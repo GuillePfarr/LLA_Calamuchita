ab628b00461ed9ecd7afad3726d3c5046ba41a88
GuillePfarr <guillepfarr@hotmail.com>
2023-03-21 23:05:42 -0300

    Estilos

--- Scripts/details.js
+++ Scripts/details.js
@@ -15,20 +15,20 @@ const container = document.getElementById("details-contenedor");
 let html = "";
 html += `
     <div class details-card">
-<div class="row no-gutters">
+<div class="row">
   <div class="col-md-6">
-    <img src="${perfil[0].image}" class="details-card-img" alt="${perfil[0].name}">
+    <img src="${perfil[0].image}" class="details-card-img"  alt="${perfil[0].name}">
   </div>
   <div class="col-md-6">
     <div class="card-body">
     <ul class="details-card-ul" >
-    <li> <span class="details-card-title">Name: ${perfil[0].name}</span></li>
+    <li> <span class="details-card-title"> ${perfil[0].name}</span></li>
     <li>Date:${perfil[0].date}</li>
     <li>Description:${perfil[0].description}</li>
     <li>Category:${perfil[0].category}</li>
     <li>Place:${perfil[0].place}</li>
     <li>Capacity:${perfil[0].capacity}</li>
-    <li>Assistance:${perfil[0].assistance}</li>
+    <li>Assistance/Estimate:${perfil[0].assistance || perfil[0].estimate}</li>
     <li>Price &#36;${perfil[0].price}</li>
    </ul>
     </div>
